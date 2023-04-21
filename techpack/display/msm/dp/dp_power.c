f3106e34ebe28beee6a34ef3f08dce7fbd30230c
DtHnAme <1904685725@qq.com>
2023-04-21 15:31:04 +0800

    techpack: display: dp: switch aux with gpio

--- techpack/display/msm/dp/dp_power.c
+++ techpack/display/msm/dp/dp_power.c
@@ -397,7 +397,7 @@ static bool dp_power_find_gpio(const char *gpio1, const char *gpio2)
 	return !!strnstr(gpio1, gpio2, strlen(gpio1));
 }
 
-static void dp_power_set_gpio(struct dp_power_private *power, bool flip)
+static void dp_power_set_gpio(struct dp_power_private *power, bool flip, bool enable)
 {
 	int i;
 	struct dss_module_power *mp = &power->parser->mp[DP_CORE_PM];
@@ -406,13 +406,16 @@ static void dp_power_set_gpio(struct dp_power_private *power, bool flip)
 	for (i = 0; i < mp->num_gpio; i++) {
 		if (dp_power_find_gpio(config->gpio_name, "aux-sel"))
 			config->value = flip;
+		if (dp_power_find_gpio(config->gpio_name, "usbplug-cc"))
+			config->value = enable;
 
 		if (gpio_is_valid(config->gpio)) {
 			DP_DEBUG("gpio %s, value %d\n", config->gpio_name,
 				config->value);
 
 			if (dp_power_find_gpio(config->gpio_name, "aux-en") ||
-			    dp_power_find_gpio(config->gpio_name, "aux-sel"))
+			    dp_power_find_gpio(config->gpio_name, "aux-sel") ||
+				dp_power_find_gpio(config->gpio_name, "usbplug-cc"))
 				gpio_direction_output(config->gpio,
 					config->value);
 			else
@@ -443,8 +446,9 @@ static int dp_power_config_gpios(struct dp_power_private *power, bool flip,
 			return rc;
 		}
 
-		dp_power_set_gpio(power, flip);
+		dp_power_set_gpio(power, flip, enable);
 	} else {
+		dp_power_set_gpio(power, flip, enable);
 		for (i = 0; i < mp->num_gpio; i++) {
 			if (gpio_is_valid(config[i].gpio)) {
 				gpio_set_value(config[i].gpio, 0);
